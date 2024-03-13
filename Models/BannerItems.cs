62015207d7f1269f5c8e61b8b81a0fdd0437cce7
FATINI <nrlftni99@gmail.com>
2024-03-13 15:15:09 +0800

    Add project files.

---
+++ Models/BannerItems.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InsuranceWebApps.Models
+{
+    public class BannerModel
+    {
+        public List<BannerItem> BannerItems { get; set; }
+    }
+
+    public class BannerItem
+    {
+        public string ImageUrl { get; set; }
+        public string AltText { get; set; }
+        public string Url { get; set; }
+    }
+}
